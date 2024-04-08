c6e981529aa8879b961d6c110d85fcee597f2eae
unknown <naimurrahman8392@gmail.com>
2024-04-08 06:36:26 +0600

    second commit

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,26 @@
+import LeftSideNav from "../Shared/LeftSideNav/LeftSideNav";
+import Header from "../Shared/Navbar/Header/Header";
+import Navbar from "../Shared/Navbar/Navbar";
+import RightSideNav from "../Shared/RightSideNav/RightSideNav";
 
 
 const Home = () => {
     return (
         <div>
-            <h2 className="text-3xl">This is Home</h2>
+            <Header></Header>
+            <Navbar></Navbar>
+     
+            <div className="grid grid-cols-1 md:grid-cols-4 gap-6 " >
+                <div className="border">
+                    <LeftSideNav></LeftSideNav>
+                </div>
+                <div className="md:col-span-2 border">
+                    <h2 className="text-4xl">News comming soon...</h2>
+                </div>
+                <div className="border">
+                    <RightSideNav></RightSideNav>
+                </div>
+            </div>
         </div>
     );
 };
